cd6391abacacfc6988e0b87939ee020180b7d41f
Dan Huang <dan.e.huang@gmail.com>
2017-10-19 01:59:51 -0700

    build script

---
+++ build_mycoq.sh
@@ -0,0 +1,24 @@
+pushd coq
+  echo "Building Coq..."
+#  make clean
+  (time (make -j4 > build.log; make install)) 2> time.log
+popd
+
+pushd mathcomp-1.6.1/mathcomp
+  echo "Building Mathematical Components..."
+  make clean
+  (time (make -j4 > build.log; make install)) 2> time.log
+popd 
+
+pushd mathcomp-odd-order-1.6.1/mathcomp
+  pushd field
+    echo "Building Field..."
+    make clean
+    (time (make -j4 > build.log; make install)) 2> time.log
+  popd
+  pushd odd_order
+    echo "Building Feit-Thompson..."
+    make clean
+    (time (make -j4 > build.log; make install)) 2> time.log
+  popd
+popd
