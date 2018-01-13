cdec44225663ce5bc49820f2be19d0cc924dd394
Jonathon Cai <jonathon.cai@gmail.com>
2018-01-13 11:24:30 -0800

    get rid of weighted_choice

--- ml4tputils/ml/rewrite/gen_rewrite.py
+++ ml4tputils/ml/rewrite/gen_rewrite.py
@@ -41,13 +41,6 @@ reflexivity.
 Qed.
 """
 
-def weighted_choice(weights):
-    rnd = random.random() * sum(weights)
-    for i, w in enumerate(weights):
-	rnd -= w
-	if rnd < 0:
-	    return i
-
 class CFG(object):
     def __init__(self):
         self.prod_rec = defaultdict(list)
