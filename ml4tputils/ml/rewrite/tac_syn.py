adc8b5a5fb039f8782dadb937f82fac29f0febea
Dan Huang <dan.e.huang@gmail.com>
2018-01-13 21:54:55 -0800

    adding interactive loading of asts for model

--- ml4tputils/ml/rewrite/tac_syn.py
+++ ml4tputils/ml/rewrite/tac_syn.py
@@ -1,17 +1,29 @@
 import random
 
 from pycoqtop.coqtop import CoqTop
+from lib.myfile import MyFile
+from recon.parse_raw import TacStParser
 
+
+"""
+[Note]
+
+Don't forget to set environment variable of where to load the
+intermediate results.
+
+    export TCOQ_DUMP=/tmp/tcoq.log
+"""
 
 # 1. Replace random policy (left, right) with learned policy
-#    a. fix tcoq to print AST table after each sendone
-#    b. use existing stuff to parse context + AST to feed into
+#    a. [DONE] fix tcoq to print AST table after each sendone
+#    b. [DONE] use existing stuff to parse context + AST to feed into
 #       tac pred + pos eval model
+#    c. Load pretrained model. Use it to do inference. Feed in next state.
 # 2. [DONE] interface with coqtop
 # 3. [DONE] sample from policy, send to coqtop, parse result
 # 4. Extension is to write which part of goal the model is attending
-# 5. Load pretrained model. Use it to do inference. Feed in next state.
-# 6. Generate dataset without myrewrite from dataset with myrewrite
+# 5. Generate dataset without myrewrite from dataset with myrewrite
+# 6. Train model
 
 
 PREFIX = """(* The set of the group. *)
@@ -43,6 +55,7 @@ def policy():
 
 class MyAlgRewriter(object):
     def __init__(self, lemma):
+        self.ts_parser = TacStParser("/tmp/tcoq.log")
         self.top = CoqTop()
         self.top.__enter__()
 
@@ -57,27 +70,52 @@ class MyAlgRewriter(object):
     def finalize(self):
         self.top.__exit__()        
 
+    def _log(self, msg):
+        print(msg)
+
+    def is_success(self, msg):
+        return "Error" not in msg
+
+    def load_tcoq_result(self):
+        # TODO(deh): can optimize to not read whole file
+        # NOTE(deh): export TCOQ_DUMP=/tmp/tcoq.log
+        ts_parser = TacStParser("/tmp/tcoq.log")
+        lemma = ts_parser.parse_partial_lemma()
+        
+        # Return decoder, contex, and conclusion
+        decl = lemma.decls[-1]
+        return lemma.decoder, decl.ctx, decl.concl_idx
+
     def proof_complete(self):
         # NOTE(deh): only works for straight-line proofs
         res = self.top.sendone("reflexivity.")
-        if "Error" not in res:
+        if self.is_success(res):
             self.top.sendone("Qed.")
             return True
         else:
             return False
-        # return "Attempt to save an incomplete proof" not in res
+
+    def _dump_ctx(self, decoder, ctx, concl_idx):
+        self._log("CTX")
+        for ident, typ_idx in ctx.traverse():
+            self._log(ident, typ_idx, decoder.decode_exp_by_key(typ_idx))
+        self._log("CONCL", concl_idx, decoder.decode_exp_by_key(concl_idx))
 
     def attempt_proof_step(self):
         self.num_steps += 1
         choice = policy()
         if choice == "RIGHT":
-            print("Trying Right")
+            self._log("Trying RIGHT")
             res = self.top.sendone("rewrite id_r.")
         else:
-            print("Trying Left")
+            self._log("Trying RIGHT")
             res = self.top.sendone("rewrite id_l.")
-        print(res)
-        return "Error" not in res
+        self._log(res)
+
+        decoder, ctx, concl_idx = self.load_tcoq_result()
+        self._dump_ctx(decoder, ctx, concl_idx)
+        
+        return self.is_success(res), decoder, ctx, concl_idx
 
     def attempt_proof(self):
         while not self.proof_complete():
