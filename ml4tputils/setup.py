58935065f5e609a591cef37d57d20437aed7eafc
Dan Huang <dan.e.huang@gmail.com>
2018-02-19 11:01:19 -0800

    setup stuff

--- ml4tputils/setup.py
+++ ml4tputils/setup.py
@@ -2,5 +2,6 @@ from setuptools import setup, find_packages
 setup(
     name="ml4tputils",
     version="0.1",
-    packages=find_packages(),
+    packages=find_packages(), install_requires=['sexpdata', 'plotly', 'matplotlib', 'networkx', 'numpy', 'scipy',
+                                                'torch']
 )
