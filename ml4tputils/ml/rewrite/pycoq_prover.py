eff374669bbea0215dce267f600c81874223cc69
Dan Huang <dan.e.huang@gmail.com>
2018-02-20 15:39:49 -0800

    refactoring solver

--- ml4tputils/ml/rewrite/pycoq_prover.py
+++ ml4tputils/ml/rewrite/pycoq_prover.py
@@ -2,45 +2,6 @@ from pycoqtop.coqtop import CoqTop
 from recon.tacst_parser import TacStParser
 
 
-PREFIX = """Require Import mathcomp.ssreflect.ssreflect.
-
-(* The set of the group. *)
-Axiom G : Set.
-
-(* The left identity for +. *)
-Axiom e : G.
-
-(* The right identity for +. *)
-Axiom m : G.
-
-(* + binary operator. *)
-Axiom f : G -> G -> G.
-
-(* For readability, we use infix <+> to stand for the binary operator. *)
-Infix "<+>" := f (at level 50).
-
-(* [m] is the right-identity for all elements [a] *)
-Axiom id_r : forall a, a <+> m = a.
-
-(* [e] is the left-identity for all elements [a] *)
-Axiom id_l : forall a, e <+> a = a.
-
-
-Ltac surgery dir e1 e2 :=
-  match goal with
-  | [ |- _ ] =>
-    let H := fresh in
-    (have H : e1 = e2 by repeat (rewrite dir); reflexivity); rewrite H; clear H
-  end.
-
-(*
-Ltac surgery dir e1 e2 :=
-  let H := fresh in
-  (have H : e1 = e2 by repeat (rewrite dir); reflexivity); rewrite H; clear H.
-*)
-"""
-
-
 class PyCoqProver(object):
     """A Python prover that constructs simple Coq proofs.
     """
