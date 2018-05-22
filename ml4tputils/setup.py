81925c222c831b79c9c62eec86d79875a25ca7fc
Dan Huang <dan.e.huang@gmail.com>
2018-05-22 13:08:04 -0700

    adding requirement

--- ml4tputils/setup.py
+++ ml4tputils/setup.py
@@ -3,5 +3,5 @@ setup(
     name="ml4tputils",
     version="0.1",
     packages=find_packages(), install_requires=['sexpdata', 'plotly', 'matplotlib', 'networkx', 'numpy', 'scipy',
-                                                'torch', 'pandas', 'psutil', 'apted']
+                                                'torch', 'pandas', 'psutil', 'apted', 'editdistance']
 )
