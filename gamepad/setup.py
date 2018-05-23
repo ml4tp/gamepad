db103aba7d6b137152a59cf150fe9e9b3ac4be55
Dan Huang <dan.e.huang@gmail.com>
2018-05-23 00:03:21 -0700

    updating requirements

--- gamepad/setup.py
+++ gamepad/setup.py
@@ -2,6 +2,16 @@ from setuptools import setup, find_packages
 setup(
     name="gamepad",
     version="0.1",
-    packages=find_packages(), install_requires=['sexpdata', 'plotly', 'matplotlib', 'networkx', 'numpy', 'scipy',
-                                                'torch', 'pandas', 'psutil', 'apted', 'editdistance']
+    packages=find_packages(),
+    install_requires=['apted',
+                      'editdistance'
+                      'matplotlib',
+                      'networkx',
+                      'numpy',
+                      'pandas',
+                      'plotly',
+                      'psutil',
+                      'scipy',
+                      'torch'
+                      ]
 )
