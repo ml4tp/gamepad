29190061b5c84dbb763ec01ef18e8a84e3687f12
Dan Huang <dan.e.huang@gmail.com>
2018-05-23 10:56:30 -0700

    updating setup and requriements

--- gamepad/setup.py
+++ gamepad/setup.py
@@ -4,7 +4,7 @@ setup(
     version="0.1",
     packages=find_packages(),
     install_requires=['apted',
-                      'editdistance'
+                      'editdistance',
                       'matplotlib',
                       'networkx',
                       'numpy',
